976fcc1294693e898f3938dee4aae25ab9306a61
Jerico Pingul <jerico.pingul@gmail.com>
2022-10-25 22:43:09 +0100

    Test publish

---
+++ .changeset/twelve-cooks-smoke.md
@@ -0,0 +1,6 @@
+---
+'@jericopingul/my-other-package': major
+'@jericopingul/my-nx-plugin': minor
+---
+
+my-other-package major nx-plugin-minor no explicit bump for hello-tsc
--- packages/my-other-package/src/lib/my-other-package.spec.ts
+++ packages/my-other-package/src/lib/my-other-package.spec.ts
@@ -2,6 +2,6 @@ import { myOtherPackage } from './my-other-package';
 
 describe('myOtherPackage', () => {
   it('should work', () => {
-    expect(myOtherPackage()).toEqual('my-other-package update 14');
+    expect(myOtherPackage()).toEqual('my-other-package update 15');
   });
 });
